6c749a2d7328d193f729f99a40597432246e83d7
github username <github email address>
2019-02-13 13:51:51 -0500

    still debugging nil

--- lib/command_line.rb
+++ lib/command_line.rb
@@ -78,19 +78,26 @@ def favorite_coffee(coffee_array, user)
 
       c1 = CoffeeShop.find_by(name: shop_name)
 
-      binding.pry
-      if Favorite.find_by(user_id: find_user_id, coffee_shop_id: c1.id)
+        if Favorite.find_by(user_id: find_user_id, coffee_shop_id: c1.id)
+          puts "You have already have this in your favorites"
+          menu
+
+      else
+        # Favorite.find_by(user_id: find_user_id, coffee_shop_id: c1.id)
 
         CoffeeShop.find_or_create_by(name: shop_name, location: shop_location)
 
         Favorite.find_or_create_by(name: shop_name, location: shop_location, user_id: find_user_id, coffee_shop_id: CoffeeShop.find_by(name: shop_name).id)
         menu
-
-      else
-        Favorite.find_by(user_id: User.find_by(name: user).id, coffee_shop_id: c1.id)
-      puts "You have already have this in your favorites"
+      end
+    elsif answer == "no"
       menu
+    else
+      error_message
+      favorite_coffee(coffee_array, user)
+    
     end
+  end
       #
       #   binding.pry
       # CoffeeShop.find_or_create_by(name: shop_name, location: shop_location)
@@ -98,14 +105,10 @@ def favorite_coffee(coffee_array, user)
       # Favorite.find_or_create_by(name: shop_name, location: shop_location, user_id: find_user_id, coffee_shop_id: CoffeeShop.find_by(name: shop_name).id)
       # menu
       # end
-  elsif answer == "no"
-    menu
-  else
-    error_message
-    favorite_coffee(coffee_array, user)
-    menu
-  end
-end
+
+
+
+
   # option_1 = "#{coffee_arr[0]["coffee shop 1"]["name"]}\n #{coffee_arr[0]["coffee shop 1"]["location"]["display_address"]}"
 #
 # puts "#{coffee_arr[0]["coffee shop 1"][:name]}"
--- lib/practice.rb
+++ lib/practice.rb
@@ -1,3 +1,42 @@
+# #
+# # shops = CoffeeShop.all.each do |shop|
+# #   puts "name: #{coffeeshop.name}, location: #{coffeeshop.location} "
+# def favorite_coffee(coffee_array, user)
 #
-# shops = CoffeeShop.all.each do |shop|
-#   puts "name: #{coffeeshop.name}, location: #{coffeeshop.location} "
+#   puts "\nWould you like to add a coffee to favorites?\n
+#   (YES or NO)"
+#   answer = gets.chomp
+#   if answer == "yes"
+#     puts "\n Please enter your number choice 1 - 5"
+#     selection = gets.chomp.to_i
+#
+#     shop_name = coffee_array[selection -1]["coffee shop #{selection}"][:name]
+#
+#     shop_location = coffee_array[selection -1]["coffee shop #{selection}"][:location].join(" ")
+#
+#     find_user_id = User.find_by(name: user).id
+#
+#       c1 = CoffeeShop.find_by(name: shop_name)
+#
+#       binding.pry
+#       if
+#         Favorite.find_by(user_id: User.find_by(name: user).id, coffee_shop_id: c1.id)
+#           puts "You have already have this in your favorites"
+#           menu
+#
+#       else
+#         # Favorite.find_by(user_id: find_user_id, coffee_shop_id: c1.id)
+#
+#         CoffeeShop.find_or_create_by(name: shop_name, location: shop_location)
+#
+#         Favorite.find_or_create_by(name: shop_name, location: shop_location, user_id: find_user_id, coffee_shop_id: CoffeeShop.find_by(name: shop_name).id)
+#         menu
+#       end
+#     elsif answer == "no"
+#       menu
+#     else
+#       error_message
+#       favorite_coffee(coffee_array, user)
+#       menu
+#     end
+#   end
